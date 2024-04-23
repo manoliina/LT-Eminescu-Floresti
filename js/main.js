4fd29786f50233a91a88ae4b992a32a22a2af087
manoliina <152322145+manoliina@users.noreply.github.com>
2024-04-23 18:39:35 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,36 @@
+new SimpleLightbox({elements: ".galerie-container a"});
+new SimpleLightbox({elements: ".orar-imagini a"});
+AOS.init({once: true});
+emailjs.init("DxCduMsOECC8mVJ9w");
+function mobileMenu() {
+    var x = document.getElementById("navbar");
+
+    if (x.className === "") {
+        x.className = "mobile";
+    } else {
+        x.className = "";
+    }
+}
+
+function send() {
+
+    if (document.getElementById("formular").checkValidity() == false) {
+        document.getElementById("formular").reportValidity();
+        return;
+    }
+
+    var date = {
+        nume: document.getElementById("nume").value,
+        email: document.getElementById("email").value,
+        mesaj: document.getElementById("mesaj").value
+    };
+
+    emailjs.send("service_ypsfaqm", "template_1omrori", date)
+        .then(function (raspuns) {
+            alert("Mesajul a fost transmis.")
+            document.getElementById("formular").reset();
+        }, function (error) {
+            alert("Eroare la transmitere. Contactati programatorul.")
+        })
+
+}
